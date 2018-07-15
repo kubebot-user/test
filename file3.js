b83a1e8c7aec4c65faa8bd4ab87999ec0a7e0f7c
kubebot-user <anshuman.bhartiya+kubebot@gmail.com>
2018-07-15 02:34:11 -0700

    Update file3.js

--- file3.js
+++ file3.js
@@ -1,10 +1,10 @@
 const FOO_BAR_SECRET_LOOKS_LIKE_KEY_ID = 'zJd-55qmsY6LD53CRTqnCr_g-';
 
 module.exports = {
-  getSecret: () => 'gm5yb-hJWRoS7ZJTi_YUj_tbU',
+  getSecret: () => 'yolo',
   secrets: {
-    KEY_IDENTIFIER: 'GxC56B6x67anequGYNPsW_-TL',
-    'MLTk-BuGS8s6Tx9iK5zaL8a_W': '2g877BA_TsE-WoPoWrjHah9ta',
+    KEY_IDENTIFIER: 'yolo',
+    'yolo': 'yolo',
     foobar: FOO_BAR_SECRET_LOOKS_LIKE_KEY_ID,
     test: 'hello-world'
   }
